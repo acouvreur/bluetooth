6a79b78254e88e2c1a24f02b9b05b3b1f41055b2
Alexis Couvreur <alexiscouvreur.pro@gmail.com>
2026-03-03 11:30:48 -0500

    fix: add EnableNotificationsWithContext darwin

--- gattc_darwin.go
+++ gattc_darwin.go
@@ -255,6 +255,15 @@ func (c DeviceCharacteristic) WriteWithoutResponseWithContext(ctx context.Contex
 // changes.
 // Users may call EnableNotifications with a nil callback to disable notifications.
 func (c DeviceCharacteristic) EnableNotifications(callback func(buf []byte)) error {
+	return c.EnableNotificationsWithContext(context.Background(), callback)
+}
+
+// EnableNotifications enables notifications in the Client Characteristic
+// Configuration Descriptor (CCCD). This means that most peripherals will send a
+// notification with a new value every time the value of the characteristic
+// changes.
+// Users may call EnableNotifications with a nil callback to disable notifications.
+func (c DeviceCharacteristic) EnableNotificationsWithContext(ctx context.Context, callback func(buf []byte)) error {
 	// If callback is nil, disable notifications
 	if callback == nil {
 		c.service.device.prph.SetNotify(false, c.characteristic)
